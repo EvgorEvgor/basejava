c682a89733b2d52c920f6a8b73a1e0df29524fa1
Evgeniy Gorodniy <evgorevgor@yandex.ru>
2018-07-26 10:42:41 +0300

    Homework 1, first step

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,70 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
+
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+  private Resume[] storage = new Resume[10000];
+  private int size = 0;
 
-    void clear() {
+  void clear() {
+    Arrays.fill(storage, 0, size, null);
+    size = 0;
+  }
+
+  void save(Resume r) {
+    int indexUuid = getIndexUuid(r.uuid);
+
+    if (indexUuid < 0) {
+      storage[size] = r;
+      size = size + 1;
+    }
+  }
+
+  Resume get(String uuid) {
+    int indexUuid = getIndexUuid(uuid);
+
+    if (indexUuid < 0) {
+      return null;
+    }
+    return storage[indexUuid];
+  }
+
+  void delete(String uuid) {
+    int indexUuid = getIndexUuid(uuid);
+
+    if (indexUuid < 0) {
+      return;
     }
 
-    void save(Resume r) {
+    int lastIndex = size - 1;
+    if (indexUuid != lastIndex) {
+      int lengthDest = lastIndex - indexUuid;
+      System.arraycopy(storage, indexUuid + 1, storage, indexUuid, lengthDest);
+    }
+    storage[lastIndex] = null;
+    size = size - 1;
+  }
+
+  /**
+   * @return array, contains only Resumes in storage (without null)
+   */
+  Resume[] getAll() {
+    return Arrays.copyOf(storage, size);
+  }
+
+  int size() {
+    return this.size;
+  }
+
+  private int getIndexUuid(String uuid) {
+    for (int i = 0; i < size; i++) {
+      if (storage[i].uuid == uuid) {
+        return i;
+      }
     }
 
-    Resume get(String uuid) {
-        return null;
-    }
-
-    void delete(String uuid) {
-    }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    Resume[] getAll() {
-        return new Resume[0];
-    }
-
-    int size() {
-        return 0;
-    }
+    return -1;
+  }
 }
